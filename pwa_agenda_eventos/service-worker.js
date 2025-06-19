cee1118fdcb92942444a193d0f5bd884d41803d2
Hugowey <vitorhugosanchez7770@gmail.com>
2025-06-19 13:01:15 -0300

    Add files via upload

---
+++ pwa_agenda_eventos/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'agenda-eventos-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
